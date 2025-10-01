1e9d82abc0cb3fd59054c13f949b99c57794d96d
vupham-308 <ptv.vupham@gmail.com>
2025-10-01 21:29:09 +0700

    Init project and account class

---
+++ src/test/java/com/ev/evchargingsystem/EvChargingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev.evchargingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
